3ae219e9387c89bdf2efd3f5a31f045fc292401d
Rafael Santos <62575388+Chavato@users.noreply.github.com>
2023-07-23 13:04:50 -0300

    feat: update product

--- CleanArch.WebUI/Controllers/ProductsController.cs
+++ CleanArch.WebUI/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
+using CleanArch.Application.DTOs;
 using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArch.WebUI.Controllers
@@ -8,11 +10,13 @@ namespace CleanArch.WebUI.Controllers
     {
         private readonly ILogger<ProductsController> _logger;
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(ILogger<ProductsController> logger, IProductService productService)
+        public ProductsController(ILogger<ProductsController> logger, IProductService productService, ICategoryService categoryService)
         {
             _logger = logger;
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -20,6 +24,48 @@ namespace CleanArch.WebUI.Controllers
         {
             var products = await _productService.GetProducts();
             return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var productDto = await _productService.GetById(id);
+            if (productDto == null) return NotFound();
+
+            var categories = await _categoryService.GetCategories();
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

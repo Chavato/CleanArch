2293c5d00143f6cfc5818019850b4aa3104766be
Rafael Santos <62575388+Chavato@users.noreply.github.com>
2023-07-24 21:47:02 -0300

    feat: get category by id

--- CleanArch.API/Controllers/CategoriesController.cs
+++ CleanArch.API/Controllers/CategoriesController.cs
@@ -27,5 +27,17 @@ namespace CleanArch.API.Controllers
 
             return Ok(categories);
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Category not found.");
+            }
+
+            return Ok(category);
+        }
     }
 }
